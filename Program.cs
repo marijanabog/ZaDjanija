fa41c4644a232c40ec648d8f7c331bc7ec100998
Marijana <Marijana@DESKTOP-3VFEPJU>
2022-08-11 13:36:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello,Djani");
+Console.WriteLine("PLEASE Enter your first name: ");
+
+string firstName = Console.ReadLine();
+Console.WriteLine("Hello " + firstName);
+
+Console.WriteLine("Please, Enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Nice to meet you " + firstName + lastName);
+
+
+
+
+
+
+
+
+
+
