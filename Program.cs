34c5cc336a456ff18b34ac3bc3992c3ba8197f87
Satara Marijana <marijana.satara@outlook.com>
2022-08-12 12:59:03 +0200

    Loop 1-100

--- Program.cs
+++ Program.cs
@@ -1,21 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,Djani");
-Console.WriteLine("PLEASE Enter your first name: ");
 
-string firstName = Console.ReadLine();
-Console.WriteLine("Hello " + firstName);
+//Console.WriteLine("PLEASE Enter your first name: ");
+//string firstName = Console.ReadLine();
+//firstName = firstName.ToLower();
 
-Console.WriteLine("Please, Enter your last name: ");
-string lastName = Console.ReadLine();
+//Console.WriteLine("Please, Enter your last name: ");
+//String lastName = Console.ReadLine();
+//lastName = lastName.ToUpper();
 
-Console.WriteLine("Nice to meet you " + firstName + lastName);
+//Console.WriteLine($"Nice to meet you {firstName} {lastName}!");
 
-
-
-
-
-
-
+Console.WriteLine("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+int i = 0;
 
 
+    while (i < max) 
+    { 
+        Console.WriteLine(i);
+    i++; 
+}
 
+Console.WriteLine("Loop finished!");
